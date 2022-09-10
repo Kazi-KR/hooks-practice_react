faf8d94049155f893d348d79b99a8eb5856c3b39
Kazi-KR <kazirabbi04@gmail.com>
2022-09-10 16:22:56 +0600

    hooks practice react_1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Search from "./Component/Search";
+//import Accordion from "./Component/Accordion";
+
+// const items=[
+//     {
+//         title:"What is React?",
+//         content:'React is a front end Javascript framework'
+//     },
+//     {
+//         title:"Why use React",
+//         content:"React gives us flexibility to reuse code"
+//     },
+//     {
+//         title:"How do you use react?",
+//         content:"You use react by creating components"
+//     }
+// ];
+
+const App=()=>{
+    return(
+        <div>
+            <Search/>
+        </div>
+    )
+}
+
+
+export default App;
